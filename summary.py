79ee69be095de97bc5572f6c20b1903a7e3cdf9b
ffallrain <ffallrain@163.com>
2023-07-21 14:34:53 +0800

    Default

--- summary.py
+++ summary.py
@@ -16,7 +16,7 @@ for line in open("all.csv"):
     ofp.write( "\t".join([p_name,o_name,p,o,start,table,flag]) )
     ofp.write( "\n" )
 
-    if flag in ("China","Taiwan","Hong Kong","Macao","ERROR"):
+    if flag in ("China","Taiwan","Hong Kong","Macao","ERROR","中国","香港","台湾","澳门"):
         continue
 
 
